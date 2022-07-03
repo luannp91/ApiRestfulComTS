75fd88df2599ce1804afe284faa86d0f9c196046
Luan Pinto <luan.nelmo@hotmail.com>
2022-07-02 22:00:08 -0300

    Resgatando Filme por Id

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { createMovie } from "./controllers/movieControllers";
+import { createMovie, findMovieById } from "./controllers/movieControllers";
 import { validate } from "./Middleware/handleValidation";
 import { movieCreateValidation } from "./Middleware/movieValidation";
 
@@ -7,4 +7,4 @@ const router = Router();
 
 export default router.get("/test", (req: Request, res: Response) => {
     res.status(200).send("API Working!!!");
-}).post("/movie", movieCreateValidation(), validate, createMovie)
+}).post("/movie", movieCreateValidation(), validate, createMovie).get("/movie/:id", findMovieById)
