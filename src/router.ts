ef1e885c16c97d3a6e4862c4888ebb0208011d38
Luan Pinto <luan.nelmo@hotmail.com>
2022-07-02 22:07:58 -0300

    Resgatando Todos os Filmes

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { createMovie, findMovieById } from "./controllers/movieControllers";
+import { createMovie, findMovieById, getAllMovies } from "./controllers/movieControllers";
 import { validate } from "./Middleware/handleValidation";
 import { movieCreateValidation } from "./Middleware/movieValidation";
 
@@ -7,4 +7,6 @@ const router = Router();
 
 export default router.get("/test", (req: Request, res: Response) => {
     res.status(200).send("API Working!!!");
-}).post("/movie", movieCreateValidation(), validate, createMovie).get("/movie/:id", findMovieById)
+}).post("/movie", movieCreateValidation(), validate, createMovie)
+    .get("/movie/:id", findMovieById)
+    .get("/movie", getAllMovies)
